b14908cb3cf0a1ba8144f7dcfb17c4140d8e106b
Mahmoud Abd Elnaser <mahmoudabdelnaser248@gmail.com>
2026-06-21 08:18:37 +0300

    implement matches POST and GET route

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import {matchRouter} from "./routes/matches.js";
 
 const app = express();
 const port = 8000;
@@ -8,6 +9,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.json({ message: "Welcome to the Sportz API!" });
 });
+
+app.use('/api/matches', matchRouter);
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
---
+++ src/routes/matches.js
@@ -0,0 +1,52 @@
+import { Router } from "express";
+import {createMatchSchema, listMatchesQuerySchema} from "../validation/matches.js";
+import {db} from "../db/db.js";
+import {getMatchStatus} from "../utils/match-status.js";
+import {matches} from "../db/schema.js";
+import {desc} from "drizzle-orm";
+
+export const matchRouter = Router();
+
+const MAX_LIMIT = 100;
+
+matchRouter.get('/', async (req, res) => {
+    const parsed = listMatchesQuerySchema.safeParse(req.query)
+    if (!parsed.success) {
+        res.status(400).json({ message: "Invalid match data", errors: JSON.stringify(parsed.error) });
+        return;
+    }
+    const limit = Math.min(parsed.data.limit ?? 50, MAX_LIMIT);
+    try {
+    const data = await db
+        .select()
+        .from(matches)
+        .orderBy((desc(matches.createdAt)))
+        .limit(limit);
+    res.status(200).json({ message: "Matches retrieved successfully", data:data });
+    } catch (e) {
+        res.status(500).json({ message: "Failed to retrieve matches" , error: e.message });
+    }
+});
+
+matchRouter.post('/', async (req, res) => {
+    const parsed = createMatchSchema.safeParse(req.body)
+    const { startTime, endTime, homeScore, awayScore } = parsed.data;
+    if (!parsed.success) {
+        res.status(400).json({ message: "Invalid match data", errors: JSON.stringify(parsed.error) });
+        return;
+    }
+    try {
+    const [event] = await db.insert(matches).values({
+        ...parsed.data,
+        startTime: new Date(startTime),
+        endTime: new Date(endTime),
+        homeScore:homeScore ?? 0,
+        awayScore:awayScore ?? 0,
+        status:getMatchStatus(startTime,endTime)
+    }).returning()
+
+        res.status(201).json({ message: "Match created successfully", data:event })
+    } catch (e) {
+        res.status(500).json({ message: "Internal server error" , error: e.message });
+    }
+});
